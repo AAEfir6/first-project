6edf863914238a3164d4e384eb953fb6b0ef4720
Walton001 <91596396+Walton001@users.noreply.github.com>
2021-09-30 13:45:09 +0800

    Add files via upload

---
+++ Task 2 Walton.py
@@ -0,0 +1,7 @@
+a = int(input('please enter the first number'))
+b = int(input('please enter the second number'))
+c = int(input('please enter the third number'))
+
+sum=float(a) + float(b)+ float(c)
+
+print('a={0} b={1} c={2} ,the sum of them is {3}'.format(a,b,c,sum))
